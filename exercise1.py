1b260c72307159b49825986c7011d96bbf6da0f9
HexScript <jarkkoph@gmail.com>
2025-12-09 12:50:24 +0200

    Updating execises

--- exercise1.py
+++ exercise1.py
@@ -1,5 +1,6 @@
 name = input("What is your name? ")
+birth_year = input("Enter your birth year? ")
+age = 2025 - int(birth_year)
 
 print("Hi " + name)
-print("Hi " + name)
-print("Hi " + name)
+print(f"Your age is {age}")
