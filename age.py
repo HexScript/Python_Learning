d0290d642d16081b9ae8a0a18a11110ea80bbd47
HexScript <jarkkoph@gmail.com>
2025-12-09 11:18:38 +0200

    Updating files

---
+++ age.py
@@ -0,0 +1,12 @@
+age = 20
+price = 19.95
+first_name = "John"
+is_online = True
+print(age)
+print(type(age))
+print(price)
+print(type(price))
+print(first_name)
+print(type(first_name))
+print(is_online)
+print(type(is_online))
